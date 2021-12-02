addb94c60f6bb528512b71b3eeb0e21c77bef1fc
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2021-12-02 12:04:08 +0530

    select location activity v1.1

---
+++ app/src/main/java/com/example/reminderapp/select_location.java
@@ -0,0 +1,14 @@
+package com.example.reminderapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_location extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_location);
+    }
+}
