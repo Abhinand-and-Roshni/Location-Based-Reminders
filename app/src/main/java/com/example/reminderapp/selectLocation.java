281f9640856e21c1f0800fc8ce80285f2dc40f01
Roshni-Bala <Roshni-Bala@users.noreply.github.com>
2021-12-04 16:47:35 +0530

    Search Map + Marker - mapview v2.1

--- app/src/main/java/com/example/reminderapp/selectLocation.java
+++ app/src/main/java/com/example/reminderapp/selectLocation.java
@@ -10,12 +10,16 @@ import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -29,6 +33,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -37,16 +42,26 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class selectLocation extends AppCompatActivity implements OnMapReadyCallback {
 
     boolean isPermissionGranted;
     GoogleMap mGoogleMap;
+    ImageView img_search;
+    EditText et_location;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_location);
+
+        img_search = findViewById(R.id.img_search);
+        et_location = findViewById(R.id.et_location);
+
         checkMyPermissions(); //location settings permission (access or deny)
 
         if(isPermissionGranted){
@@ -58,6 +73,38 @@ public class selectLocation extends AppCompatActivity implements OnMapReadyCallb
                 Toast.makeText(this, "Google PlayServices are not available", Toast.LENGTH_SHORT).show();
             }
         }
+
+        img_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String location = et_location.getText().toString();
+                if(location == null){
+                    Toast.makeText(selectLocation.this, "Type a valid location!", Toast.LENGTH_SHORT).show();
+                }else{
+                    Geocoder geocoder = new Geocoder(selectLocation.this, Locale.getDefault());
+                    try {
+                        List<Address> listAddress = geocoder.getFromLocationName(location,1 );
+                        if(listAddress.size()>0){
+                            LatLng latLng = new LatLng(listAddress.get(0).getLatitude(), listAddress.get(0).getLongitude());
+                            double lat1 = listAddress.get(0).getLatitude();
+                            double long1 = listAddress.get(0).getLongitude();
+                            String lat1s = Double.toString(lat1);
+                            String long1s = Double.toString(long1);
+                            Toast.makeText(selectLocation.this, lat1s + " " + long1s , Toast.LENGTH_SHORT).show();
+                            MarkerOptions markerOptions = new MarkerOptions();
+                            markerOptions.title("Here");
+                            markerOptions.position(latLng);
+                            mGoogleMap.addMarker(markerOptions);
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 5);
+                            mGoogleMap.animateCamera(cameraUpdate);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+        });
 
     }
 
@@ -109,6 +156,8 @@ public class selectLocation extends AppCompatActivity implements OnMapReadyCallb
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+
 
     }
 
